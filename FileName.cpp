55ee88ad5eb33c51a3379fa032b6d30a358c9d02
kitase16 <kitasebeef@gmail.com>
2026-03-16 16:51:55 +0900

    1st

---
+++ FileName.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdint>
+
+struct CMemory {
+	typedef std::int16_t PinWord;
+	enum ALU {None,Add,Sub,Mul,Div,Mod,And,Or,Nand,Nor,Not,Xor,Nxor};
+	enum IO { In, Out };
+	enum Data { Int, Float, Char, Bool ,String};
+
+	std::intmax_t Work[512] = {};
+	std::int16_t Pool[2][128 * 1024] = { {} ,{} };
+
+	PinWord Out = 0;
+	PinWord In = 0;
+	//Random number generator. so many.
+};
+
+void Process(CMemory& mem) {
+	//Process the memory and do the operations.
+}
+
+void Setup(CMemory& mem,CMemory::PinWord PW) {
+	//Setup the memory with the program and data.
+	mem.In = PW;
+}
+void Run(CMemory& mem) {
+	Process(mem);
+	std::cout << "Output: " << mem.Out << std::endl;
+}
+
+void main() {
+	CMemory mem;
+	Setup(mem, 42); // Example input
+	Run(mem);
+}
+
