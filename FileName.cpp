1edf80175e7912932373cee3ed12b6761d39bb05
kitase16 <kitasebeef@gmail.com>
2026-03-16 19:30:23 +0900

    4th

--- FileName.cpp
+++ FileName.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <cstdint>
 
+template<class Int>
+struct PinIO{
+	Int In = {};
+	Int Out = {};
+};
+
 struct CMemory {
 	typedef std::int16_t PinWord;
 	enum ALU {None,Add,Sub,Mul,Div,Mod,And,Or,Nand,Nor,Not,Xor,Nxor};
 	enum IO { in, out };
 	enum Data { Int, Float, Char, Bool ,String};
 
-	std::intmax_t Work[512] = {};
+	PinIO<std::intmax_t> Work[512] = { {}, };
 	std::int16_t Pool[2][128 * 1024] = { {} ,{} };
 
-	PinWord Out = 0;
-	PinWord In = 0;
+	PinIO<PinWord> Pins;
+//	PinWord Out = 0;
+//	PinWord In = 0;
 	//Random number generator. so many.
 };
 
@@ -21,11 +28,11 @@ void Process(CMemory& mem) {
 
 void Setup(CMemory& mem,CMemory::PinWord PW) {
 	//Setup the memory with the program and data.
-	mem.In = PW;
+	mem.Pins.In = PW;
 }
 void Run(CMemory& mem) {
 	Process(mem);
-	std::cout << "Output: " << mem.Out << std::endl;
+	std::cout << "Output: " << mem.Pins.Out << std::endl;
 }
 
 int main() {
